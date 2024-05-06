704eba33741177cc133e71e89d0bc3fbc1ccb379
Shubham Lagad <shubhamlagad@gmail.com>
2024-05-05 20:34:37 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from func import get_todos, write_todos
 import time
 
-time.strftime()strftime()
+now = time.strftime("%b %d, %Y %H:%M:%S")
+
+print("It is " +now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").strip()
 
